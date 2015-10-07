690da8da847084ae8d6a5d6579d717a76c10cfce
Rinke <rinke.nuyten@hotmail.com>
2015-10-07 10:42:43 +0200

    yo
    Added Oefening main menu

--- app/src/main/java/com/firebee/se41/firebee/MainMenu.java
+++ app/src/main/java/com/firebee/se41/firebee/MainMenu.java
@@ -1,9 +1,12 @@
 package com.firebee.se41.firebee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -11,6 +14,17 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button oefeningButton = (Button)findViewById(R.id.btnTraining);
+        oefeningButton.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent oefeningWindow = new Intent(MainMenu.this, MainOefening.class);
+                startActivity(oefeningWindow);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/firebee/se41/firebee/MainOefening.java
@@ -0,0 +1,19 @@
+package com.firebee.se41.firebee;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Rinke on 7-10-2015.
+ */
+public class MainOefening extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_main);
+    }
+}
