33f8b07e55cdc7ec96e07f6cc9f1931fde51081e
Rinke <rinke.nuyten@hotmail.com>
2015-10-12 11:24:02 +0200

    Werking v/d nieuwe training knop

--- app/src/main/java/com/firebee/se41/firebee/MainOefening.java
+++ app/src/main/java/com/firebee/se41/firebee/MainOefening.java
@@ -1,10 +1,13 @@
 package com.firebee.se41.firebee;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Rinke on 7-10-2015.
@@ -15,5 +18,16 @@ public class MainOefening extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_main);
+
+
+        Button btnNieuwTraining = (Button) findViewById(R.id.btnNewTraining);
+        btnNieuwTraining.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent nieuwTrainingWindow = new Intent(MainOefening.this, NieuweOefeningActivity.class);
+                startActivity(nieuwTrainingWindow);
+            }
+        });
     }
 }
